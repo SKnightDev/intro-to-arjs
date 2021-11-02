09a2e750bc6056204a6e0db090b8cd484fa0f24b
Stewart Knight <stewart.knight@phygital.co.uk>
2021-11-02 15:19:25 +0000

    Added init function and empty functions

---
+++ js/game.js
@@ -0,0 +1,31 @@
+﻿{
+    const image = new Image();
+    const takePhotoButton = document.querySelector('.takePhoto');
+    
+    let constraints, imageCapture, mediaStream, video;
+    
+    const init = () => {
+        video = document.querySelector('video');
+        navigator.mediaDevices.enumerateDevices()
+            .catch(error => console.log('enumerateDevices() error', error))
+            .then(getStream);
+        
+        takePhotoButton.addEventListener('click', getPicture);
+    }
+    
+    // get video stream from the camera
+    const getStream = () => {
+        
+    };
+    
+    // displays stream from the camera, and then creates an ImageCapture object, using the video stream
+    const gotStream = stream => {
+        
+    };
+    
+    // take the picture
+    const getPicture = () => {
+        
+    };
+    
+}
